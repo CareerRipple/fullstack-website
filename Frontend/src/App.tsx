7e0c590100cb851a5f4ba52711807629797e2ea5
Nansamba Ssensalo <nssensalo@gmail.com>
2024-09-10 16:16:46 -0700

    removing apache adding react,django,nginx,gunicorn,docker

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+         <h1>Welcome to the Frontend, Career Ripplers!</h1>
+      </div>
+      
+      
+      
+    </>
+  )
+}
+
+export default App
