d971b29684393da1f9ac904dbd745601265a6e23
Nansamba Ssensalo <nssensalo@gmail.com>
2024-09-10 16:16:46 -0700

    removing apache adding react,django,nginx,gunicorn,docker

---
+++ Frontend/src/main.tsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { BrowserRouter as Router } from 'react-router-dom';
+
+createRoot(document.getElementById('root')!).render(
+  <Router>
+      <StrictMode>
+        <App />
+      </StrictMode>
+  </Router>
+  
+)
